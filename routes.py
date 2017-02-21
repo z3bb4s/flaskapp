73e21c7929df57d31b4d1206c6d602ddec7c9ed8
z3b <z3b@x220>
2017-02-21 19:39:55 +0100

    testar flask

--- routes.py
+++ routes.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/portfolio")
+def portfolio():
+	return render_template("portfolio.html")
+
+@app.route("/contact")
+def contact():
+	return render_template("contact.html")
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
